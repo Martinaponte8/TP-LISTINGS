190ab89272914e59ef690000e08da3ef907fdf3a
FNVG <87998232+FNVG@users.noreply.github.com>
2021-09-13 08:57:43 -0400

    Add files via upload

--- src/capitulo_4/4.14/condvar.c
+++ src/capitulo_4/4.14/condvar.c
@@ -1,4 +1,9 @@
 #include <pthread.h>
+#include <stdio.h>
+
+void do_work () {
+  printf("haciendo trabajo\n");
+}
 
 int thread_flag;
 pthread_cond_t thread_flag_cv;
@@ -6,47 +11,60 @@ pthread_mutex_t thread_flag_mutex;
 
 void initialize_flag ()
 {
-	/* Initialize the mutex and condition variable. */
-	pthread_mutex_init (&thread_flag_mutex, NULL);
-	pthread_cond_init (&thread_flag_cv, NULL);
-	/* Initialize the flag value. */
-	thread_flag = 0;
+  /* Inicializa el mutex y la variable de condición.  */
+  pthread_mutex_init (&thread_flag_mutex, NULL);
+  pthread_cond_init (&thread_flag_cv, NULL);
+  /* Inicializa el valor de la bandera.  */
+  thread_flag = 0;
 }
 
-/* Calls do_work repeatedly while the thread flag is set; blocks if
-the flag is clear. */
+/* Llama a do_work repetidamente mientras el hilo está on; bloquea si
+   la bandera esta limpia.  */
+
 void* thread_function (void* thread_arg)
 {
-	/* Loop infinitely. */
-	while (1) {
-	/* Lock the mutex before accessing the flag value. */
-	pthread_mutex_lock (&thread_flag_mutex);
-	while (!thread_flag)
-	/* The flag is clear. Wait for a signal on the condition
-	variable, indicating that the flag value has changed. When the
-	signal arrives and this thread unblocks, loop and check the
-	flag again. */
-	pthread_cond_wait (&thread_flag_cv, &thread_flag_mutex);
-	/* When we’ve gotten here, we know the flag must be set. Unlock
-	the mutex. */
-	pthread_mutex_unlock (&thread_flag_mutex);
-	/* Do some work. */
-	do_work ();
-	}
-	return NULL;
+  /* Loop infinitamente.  */
+  while (1) {
+    /* Bloquea el mutex antes de acceder al valor de la bandera.  */
+    pthread_mutex_lock (&thread_flag_mutex);
+    while (!thread_flag) 
+      /* La bandera está limpia.  Esperando por la señal de la condición
+     de la variable, indicando que el valor ha cambiado.  Cuando la señal llegue 
+     y este hilo se desbloquee, loopear y checkear la bandera de vuelta. */
+      pthread_cond_wait (&thread_flag_cv, &thread_flag_mutex);
+    /* Cuando estamos aquí, sabemos que la bandera debe ser seteada.  Desbloquea
+       el mutex.  */
+    pthread_mutex_unlock (&thread_flag_mutex);
+    /* Hace algun trabajo.  */
+    do_work ();
+  }
+  return NULL;
 }
 
-/* Sets the value of the thread flag to FLAG_VALUE. */
+/* Setea el valor de la bandera del hilo a FLAG_VALUE.  */
+
 void set_thread_flag (int flag_value)
 {
-	/* Lock the mutex before accessing the flag value. */
-	pthread_mutex_lock (&thread_flag_mutex);
-	/* Set the flag value, and then signal in case thread_function is
-	blocked, waiting for the flag to become set. However,
-	thread_function can’t actually check the flag until the mutex is
-	unlocked. */
-	thread_flag = flag_value;
-	pthread_cond_signal (&thread_flag_cv);
-	/* Unlock the mutex. */
-	pthread_mutex_unlock (&thread_flag_mutex);
+  /* Bloquea el mutex antes de acceder al valor de la bandera.  */
+  pthread_mutex_lock (&thread_flag_mutex);
+  /* Setear el valor de la bandera, y la señal en caso de que thread_function esté
+     bloqueado, esperando ser seteado por la bandera. De todos modos,
+     thread_function no puede chekear la bandera hasta que el mutex esté desbloqueado. */
+  thread_flag = flag_value;
+  pthread_cond_signal (&thread_flag_cv);
+  /* Desbloquea el mutex.  */
+  pthread_mutex_unlock (&thread_flag_mutex);
+}
+
+int main() {
+
+  pthread_t thread_id;
+  initialize_flag();
+
+  set_thread_flag(2);
+
+  pthread_create(&thread_id, NULL, &thread_function, NULL);
+  pthread_join(thread_id, NULL);
+
+  return 0;
 }
