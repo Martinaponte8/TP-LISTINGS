5170de71a922c458fa888912bb67c67092b37824
FNVG <87998232+FNVG@users.noreply.github.com>
2021-09-13 08:55:30 -0400

    Add files via upload

--- src/capitulo_4/4.13/spin-condvar.c
+++ src/capitulo_4/4.13/spin-condvar.c
@@ -1,34 +1,57 @@
 #include <pthread.h>
+#include <stdio.h>
+
+void do_work (){
+  printf("hello\n");
+};
+
 int thread_flag;
 pthread_mutex_t thread_flag_mutex;
+
 void initialize_flag ()
 {
-pthread_mutex_init (&thread_flag_mutex, NULL);
-thread_flag = 0;
+  pthread_mutex_init (&thread_flag_mutex, NULL);
+  thread_flag = 0;
 }
 
-/* Calls do_work repeatedly while the thread flag is set; otherwise
-spins. */
+/* Llama a do_work repetidamente mientras el hilo de la bandera este on; de otro modo hace un giro.  */
+
 void* thread_function (void* thread_arg)
 {
-	while (1) {
-	int flag_is_set;
-	/* Protect the flag with a mutex lock. */
-	pthread_mutex_lock (&thread_flag_mutex);
-	flag_is_set = thread_flag;
-	pthread_mutex_unlock (&thread_flag_mutex);
-	if (flag_is_set)
-	do_work ();
-	/* Else don’t do anything. Just loop again. */
-	}
-	return NULL;
+  while (1) {
+    int flag_is_set;
+
+    /* Protege la bandera con un mutex lock.  */
+    pthread_mutex_lock (&thread_flag_mutex);
+    flag_is_set = thread_flag;
+    pthread_mutex_unlock (&thread_flag_mutex);
+
+    if (flag_is_set)
+      do_work ();
+    /* Sino no hacer nada.  Solo volver a hacer el loop.  */
+  }
+  return NULL;
 }
 
-/* Sets the value of the thread flag to FLAG_VALUE. */
+/* Setea el valor de la bandera del hilo a FLAG_VALUE.  */
+
 void set_thread_flag (int flag_value)
 {
-	/* Protect the flag with a mutex lock. */
-	pthread_mutex_lock (&thread_flag_mutex);
-	thread_flag = flag_value;
-	pthread_mutex_unlock (&thread_flag_mutex);
+  /* Protege la bandera con un mutex lock.  */
+  pthread_mutex_lock (&thread_flag_mutex);
+  thread_flag = flag_value;
+  pthread_mutex_unlock (&thread_flag_mutex);
+}
+
+int main() {
+
+  pthread_t thread_id;
+  initialize_flag();
+
+  set_thread_flag(2);
+
+  pthread_create(&thread_id, NULL, &thread_function, NULL);
+  pthread_join(thread_id, NULL);
+
+  return 0;
 }
